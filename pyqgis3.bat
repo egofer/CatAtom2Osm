4a7ea5f4986871bbef69e130e163b12009f6b145
javiersanp <javiersanp@gmail.com>
2018-06-02 21:52:54 +0000

    PY3 w$ bat, issue #7

---
+++ pyqgis3.bat
@@ -0,0 +1,18 @@
+@echo off 
+set OSGEO4W_ROOT=C:\OSGeo4W3
+@IF EXIST C:\OsGeo4W64\nul set OSGEO4W_ROOT=C:\OSGeo4W64
+@IF EXIST C:\OsGeo4W32\nul set OSGEO4W_ROOT="C:\OSGeo4W32
+path %~dp0;%OSGEO4W_ROOT%\bin;%OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
+call "%OSGEO4W_ROOT%\bin\o4w_env.bat" 
+call "%OSGEO4W_ROOT%\bin\qt5_env.bat" 
+call "%OSGEO4W_ROOT%\bin\py3_env.bat" 
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
+set GDAL_FILENAME_IS_UTF8=YES
+set VSI_CACHE=TRUE
+set VSI_CACHE_SIZE=1000000
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\Qt5\plugins
+set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python36
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;%PYTHONPATH%
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\Python36\Lib\site-packages;%PYTHONPATH%
+rem cd <path to cadastre files>
+cmd.exe
