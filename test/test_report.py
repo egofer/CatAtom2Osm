39c1d920e67e21803150945bd22941a4361a2d18
javiersanp <javiersanp@gmail.com>
2018-06-02 22:46:49 +0000

    Fix w$ test_report, issue #7

--- test/test_report.py
+++ test/test_report.py
@@ -154,8 +154,7 @@ class TestReport(unittest.TestCase):
         fn = 'test_report.txt'
         r.to_file(fn)
         with open(fn, 'r') as fo:
-            text = bytes(fo.read(), 'utf-8')
-            text = text.decode(setup.encoding)
+            text = fo.read()
             text = text.replace('\n', setup.eol)
         self.assertEqual(output, text)
         if os.path.exists(fn):
