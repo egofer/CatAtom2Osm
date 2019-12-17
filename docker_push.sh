05eaa899f53c706ba5054533c1bc0885e9fee33c
Javier Sánchez Portero <frsanchezp@aemet.es>
2019-12-17 14:23:38 +0000

    Fix docker repository name

--- docker_push.sh
+++ docker_push.sh
@@ -26,6 +26,6 @@ VERSION=$(grep app_version setup.py | cut -d"=" -f2 | tr -d " \"'")
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker push "$DOCKER_USERNAME"/prueba:latest
-docker tag "$DOCKER_USERNAME"/prueba:latest "$DOCKER_USERNAME"/prueba:"$VERSION"
-docker push "$DOCKER_USERNAME"/prueba:"$VERSION"
+docker push "$DOCKER_USERNAME"/catatom2osm:latest
+docker tag "$DOCKER_USERNAME"/catatom2osm:latest "$DOCKER_USERNAME"/prueba:"$VERSION"
+docker push "$DOCKER_USERNAME"/catatom2osm:"$VERSION"
